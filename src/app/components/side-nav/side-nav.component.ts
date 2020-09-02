7944a029a1218ca9e735bca1455c9009d1e93b97
Himanshu Sekhar Dash <hdash@evoketechnologies.com>
2020-09-02 13:25:23 +0530

    updated side-nav with json data iteration

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+
+interface Icon {
+  id: number;
+  name: string;
+  icon: string;
+}
 
 @Component({
   selector: 'ev-side-nav',
@@ -6,10 +14,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent implements OnInit {
-
-  constructor() { }
+  arrIcons: Icon[] = [];
+  constructor(private httpService: HttpClient) { }
 
   ngOnInit(): void {
+    this.httpService.get('assets/side_nav.json').subscribe(
+      data => {
+        this.arrIcons = data as Icon[];	 // FILL THE ARRAY WITH DATA.
+         console.log(this.arrIcons);
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    );
   }
 
 }

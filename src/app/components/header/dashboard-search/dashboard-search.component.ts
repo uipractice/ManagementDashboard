7c3d5bd49fd6d4951ff697f22d480b1536374403
Bhavitha Dondapati <bdondapati@evoketechnologies.com>
2020-09-04 17:04:39 +0530

    Commit CSS

--- src/app/components/header/dashboard-search/dashboard-search.component.ts
+++ src/app/components/header/dashboard-search/dashboard-search.component.ts
@@ -7,9 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardSearchComponent implements OnInit {
 
-  toggleDropdown(){
-    this.isShow = !this.isShow;
-  }
   constructor() { }
 
   ngOnInit(): void {

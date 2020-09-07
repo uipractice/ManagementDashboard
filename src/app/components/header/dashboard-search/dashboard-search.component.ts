af7397a45e16574344b180b56377b67f283f3148
Himanshu Sekhar Dash <hdash@evoketechnologies.com>
2020-09-07 21:34:09 +0530

    updated header with outside click dropdown close functionality & moved  logout button to user profile section

--- src/app/components/header/dashboard-search/dashboard-search.component.ts
+++ src/app/components/header/dashboard-search/dashboard-search.component.ts
@@ -3,12 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ev-dashboard-search',
   templateUrl: './dashboard-search.component.html',
-  styleUrls: ['./dashboard-search.component.scss']
+  styleUrls: ['./dashboard-search.component.scss'],
+  host: {'(document:click)': 'onClick()'}
 })
 export class DashboardSearchComponent implements OnInit {
   isShow = true;
-  toggleDropdown(){
+  toggleDropdown($event){
+    $event.stopPropagation();
     this.isShow = !this.isShow;
+  }
+  onClick() {
+    this.isShow = true;
   }
   constructor() { }
 

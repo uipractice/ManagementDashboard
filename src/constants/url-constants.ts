83dea7a6c1990a455c29991f428945475de4d9f6
Vinayaka Dharmasthala <vdharmasthala@evoketechnologies.com>
2020-10-14 20:47:12 +0530

    added login logic with minor changes

--- src/constants/url-constants.ts
+++ src/constants/url-constants.ts
@@ -7,8 +7,8 @@ export class UrlConstants {
     stage_URL:
       'http://ec2-35-154-46-177.ap-south-1.compute.amazonaws.com/swagger',
 
-    // DEV_URL: 'http://localhost',
-    DEV_URL: 'http://65.0.17.4',
+    DEV_URL: 'http://localhost',
+    // DEV_URL: 'http://65.0.17.4',
     ADD_USER: 'api/emp/emp',
     GET_USERS: 'api/emp/getAllEmp',
     DELETE_EMP: 'api/emp/deleteEmp',

cd07f0acb57f2c9ce8425f675321257344cbbb98
srsamal1 <ssamal1@evoketechnologies.com>
2021-01-12 19:08:19 +0530

    Adding sortable property and remove unnecessary code.

--- src/app/reusable/components/data-modal/data-modal.component.ts
+++ src/app/reusable/components/data-modal/data-modal.component.ts
@@ -29,75 +29,13 @@ export class DataModalComponent implements OnInit {
   }
   columnDefs = [
     {  headerName: "SL.No", field: 'SLNo', width: 86},
-    {  headerName: 'EMP Id',field: 'employee_employee_id', sortable: true, filter: true, width: 100 },
-    {  headerName: 'NAME',field: 'employee_company_name', sortable: true, filter: true, width: 190 },
-    {  headerName: 'DESIGNATION',field: 'employee_designation_name', sortable: true, filter: true,width: 190 },
-    {  headerName: 'PRACTICE',field: 'master3', sortable: true, filter: true},
-    {  headerName: 'REPORTING TO',field: 'employee_reporting_to', sortable: true, filter: true}
+    {  headerName: 'EMP Id',field: 'employee_employee_id', sortable: true, width: 100 },
+    {  headerName: 'NAME',field: 'employee_company_name', filter: true, width: 190 },
+    {  headerName: 'DESIGNATION',field: 'employee_designation_name',filter: true,width: 190 },
+    {  headerName: 'PRACTICE',field: 'master3', filter: true},
+    {  headerName: 'REPORTING TO',field: 'employee_reporting_to',filter: true}
 ];
-// columnDefs = [
-//     {
-//       children: [
-//         {
-//           headerName: 'Athlete',
-//           field: 'employee_employee_id',
-//           width: 150,
-//           suppressSizeToFit: true,
-//           enableRowGroup: true,
-//           rowGroupIndex: 0,
-//         },
-//         {
-//           headerName: 'Age',
-//           field: 'employee_company_name',
-//           width: 90,
-//           minwidth: 75,
-//           maxWidth: 100,
-//           enableRowGroup: true,
-//         },
-//        {
-//          headerName: 'Age',
-//          field: 'employee_department_name',
-//          width: 90,
-//          minwidth: 75,
-//          maxWidth: 100,
-//          enableRowGroup: true,
-//        },
-//        {
-//          headerName: 'Age',
-//          field: 'employee_reporting_to',
-//          width: 90,
-//          minwidth: 75,
-//          maxWidth: 100,
-//          enableRowGroup: true,
-//        },
-//        {
-//          headerName: 'Age',
-//          field: 'master3',
-//          width: 90,
-//          minwidth: 75,
-//          maxWidth: 100,
-//          enableRowGroup: true,
-//        },
-//        {
-//          headerName: 'Age',
-//          field: 'master1',
-//          width: 90,
-//          minwidth: 75,
-//          maxWidth: 100,
-//          enableRowGroup: true,
-//        }
-//       ],
-//     },
-//   ];
-  //  gridOptions = {
-  //     defaultColDef: {
-  //       sortable: true,
-  //       resizable: true,
-  //       floatingFilter: true,
-  //     },
-  //     columnDefs: this.columnDefs,
-  //     rowData: null,
-  //   };
+
   actionFunction() {
     // this.modalService.modalAction(this.modalData);
     this.closeModal();

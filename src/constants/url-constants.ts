0167c9d2bd3ae03a77d4fc4f19525affaf07350e
srsamal1 <ssamal1@evoketechnologies.com>
2021-08-25 12:57:44 +0530

    Removing meta tag in index.html file

--- src/constants/url-constants.ts
+++ src/constants/url-constants.ts
@@ -1,8 +1,8 @@
 export class UrlConstants {
   constructor() { }
-  static URLS = {
-    DEV_URL: 'https://mcapi-qa.kaakateeya.com',
-  };
+  // static URLS = {
+  //   DEV_URL: 'https://mcapi-qa.kaakateeya.com',
+  // };
   static ENDPOINTS = {
     stage_URL:
       'http://ec2-35-154-46-177.ap-south-1.compute.amazonaws.com/swagger',

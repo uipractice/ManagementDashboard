485fa95022ec75ec4cdeba554aba8572cc7c505d
srsamal1 <ssamal1@evoketechnologies.com>
2021-08-26 14:15:23 +0530

    Adding https in dev url

--- src/constants/url-constants.ts
+++ src/constants/url-constants.ts
@@ -8,7 +8,7 @@ export class UrlConstants {
       'http://ec2-35-154-46-177.ap-south-1.compute.amazonaws.com/swagger',
 
     // DEV_URL: 'http://localhost',
-    DEV_URL: 'http://13.234.227.88',
+    DEV_URL: 'https://13.234.227.88',
     ADD_USER: 'api/emp/emp',
     GET_USERS: 'api/emp/getAllEmp',
     DELETE_EMP: 'api/emp/deleteEmp',
